3641c80e4e47e1bf1bf318917091e8183fba1635
骇人的籽 <60039041+JoshuaYu-crash@users.noreply.github.com>
2021-09-13 20:27:27 +0800

    fix the bugs

--- work/main.c
+++ work/main.c
@@ -9,15 +9,7 @@
 #endif
 
 int main() {
-    // struct sigaction act; /*包含信号处理动作的结构体*/
-    // act.sa_sigaction=stopPlay;  /*指定信号处理函数*/
-    // act.sa_flags=SA_SIGINFO;   /*表明信号处理函数由sa_sigaction指定*/
-    // sigemptyset(&act.sa_mask);
-    // /*信号集处理函数，将act.sa_mask所指向的信号集清空，*/
-    // /*即不包含任何信号*/
-    // sigaction(SIGINT,&act,NULL);   /*注册SIGINT信号*/
-
+    // start game func
     startPlay();
-    
     return 0;
 }
--- work/play.c
+++ work/play.c
@@ -17,7 +17,7 @@ int userChoice;
 
 void stopPlay(int iSignNo, siginfo_t *pInfo, void *pReserved) { // log the pid to close
     if(totalNum!=0) {
-        printf("\n总共进行了%d局，玩家的胜率是%.2f%%\n游戏结束！", totalNum, (float)userWinNum/(float)totalNum);
+        printf("\n总共进行了%d局，玩家获胜%d局，玩家的胜率是%.2f%%\n游戏结束！", totalNum, userWinNum, (float)userWinNum/(float)totalNum*100);
     }
     printf("\n等待进程 %d 退出~~~\n", getpid());
     if(execv("/bin/echo", para)==-1) {
@@ -37,33 +37,36 @@ void startPlay() {
         printf("输入之后按下回车，与计算机的选择相比较\n");
         printf("输入之后按下回车，与计算机的选择相比较\n");
         printf("剪刀赢布，布赢石头，石头赢剪刀\n");
-        printf("输入Ctrl+C退出游戏，之后会结算输赢局数，可选择是否保存为文件\n");
+        printf("输入Ctrl+C退出游戏，之后会结算输赢局数，以及胜率\n");
         while(lock==0);
         exit(0);
     }
     else { // play the game
-        struct sigaction act; /*包含信号处理动作的结构体*/
-        act.sa_sigaction=stopPlay;  /*指定信号处理函数*/
-        act.sa_flags=SA_SIGINFO;   /*表明信号处理函数由sa_sigaction指定*/
+        // register the Ctrl+C to the process
+        struct sigaction act; // include the signal handler struct
+        act.sa_sigaction=stopPlay; // Specifies the signal handler function
+        act.sa_flags=SA_SIGINFO;
         sigemptyset(&act.sa_mask);
-        /*信号集处理函数，将act.sa_mask所指向的信号集清空，*/
-        /*即不包含任何信号*/
-        sigaction(SIGINT,&act,NULL);   /*注册SIGINT信号*/
+        sigaction(SIGINT,&act,NULL); // register the Ctrl+C signal
         // 0 is Rock
         // 1 is Scissor
         // 2 is Paper
         while(scanf("%d", &userChoice)!=-1) {
-            computerChoice = rand() % 3;
+            computerChoice = rand() % 3; // random the computer's choice
+            int res = userChoice - computerChoice;
+            if(userChoice > 2 || userChoice < 0) {
+                printf("玩家输入错误，请重新输入\n");
+                continue;
+            }
             totalNum++;
             printf("这是第%d局，用户的选择是%s，电脑的选择是%s\n这一局赢得是：",
                 totalNum,
                 userChoice == 0 ? "石头" : (userChoice == 1 ? "剪刀" : "布"),
                 computerChoice == 0 ? "石头" : (computerChoice == 1 ? "剪刀" : "布")
             );
-            int res = userChoice - computerChoice;
             if(res == -1 || res == 2) {
                 userWinNum++;
-                printf("用户！\n");
+                printf("玩家！\n");
             }
             else if(res == 0) {
                 printf("平局！\n");
