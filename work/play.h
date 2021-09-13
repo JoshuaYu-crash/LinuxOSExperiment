723ae8be7173b52f36f03cd2094478b66ba5f21e
骇人的籽 <60039041+JoshuaYu-crash@users.noreply.github.com>
2021-09-13 20:53:08 +0800

    update

--- work/play.h
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-#include <unistd.h>
-#include <sys/types.h>
-#include <stdlib.h>
-#include <signal.h>
-
-void startPlay();
-void stopPlay(int iSignNo, siginfo_t *pInfo, void *pReserved);
