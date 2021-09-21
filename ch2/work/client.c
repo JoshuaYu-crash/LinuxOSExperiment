b1f2cac962111028d6ba46d8177e8155fd866f20
骇人的籽 <60039041+JoshuaYu-crash@users.noreply.github.com>
2021-09-21 23:27:56 +0800

    update: fix bug

--- ch2/work/client.c
+++ ch2/work/client.c
@@ -3,27 +3,48 @@
 #define FIFO_FILE "/tmp/TEMPFIFO"/*命名管道的路径、文件名*/
 #define MAX_LEN 1024
 
-
-int main() {
-    FILE *fp;
-    int i;
-
-    if ((fp = fopen(FIFO_FILE, "w")) == NULL) {/*打开命名管道文件*/
+FILE * openfile(FILE *fp, char type[]) {
+    if ((fp = fopen(FIFO_FILE, type)) == NULL) {/*打开命名管道文件*/
         printf("open fifo failed. \n");
         exit(1);
     }
+}
 
-    while(1) {
-        printf("write : ");
-        char read_str[MAX_LEN];
-        scanf("%s", read_str);
-        if (fputs(read_str, fp) == EOF) {
-            printf("write fifo error. \n");
-            exit(1);
+int main() {
+    FILE *fp;
+    int i, pid;
+    while((pid=fork())==-1);
+    // receive msg
+    if(pid == 0) {
+        while (1) {
+            char readbuf[MAX_LEN];
+            fp = openfile(fp, "r")
+            if (fgets(readbuf, MAX_LEN, fp) != NULL) {/*从命名管道文件中读数据*/
+                printf("Received message from Server :%s \n", readbuf);
+                fclose(fp);
+            }
+            else {
+                if (ferror(fp)) {
+                    printf("read fifo failed.\n");
+                    exit(1);
+                }
+            }
         }
     }
-
-    fclose(fp);
+    // send msg
+    else {
+        while(1) {
+            fp = openfile(fp, "w");
+            printf("Wait for you send\n>> ");
+            char read_str[MAX_LEN];
+            scanf("%s", read_str);
+            if (fputs(read_str, fp) == EOF) {
+                printf("write fifo error. \n");
+                exit(1);
+            }
+            fclose(fp);
+        }
+    }
     return 0;
 
 }
--- ch2/work/server.c
+++ ch2/work/server.c
@@ -21,24 +21,40 @@ void init(FILE *fp) {
 
 int main() {
     FILE *fp;
-    char readbuf[80];
-
     init(fp);
 
-    while (1) {
-        if ((fp = fopen(FIFO_FILE, "r")) == NULL) {/*打开命名管道文件*/
-            printf("open fifo failed. \n");
-            exit(1);
-        }
 
-        if (fgets(readbuf, 80, fp) != NULL) {/*从命名管道文件中读数据*/
-            printf("Received string :%s \n", readbuf);
-            fclose(fp);
-        } else {
-            if (ferror(fp)) {
-                printf("read fifo failed.\n");
+    int i, pid;
+    while((pid=fork())==-1);
+    // receive msg
+    if(pid == 0) {
+        while (1) {
+            char readbuf[MAX_LEN];
+            fp = openfile(fp, "r")
+            if (fgets(readbuf, MAX_LEN, fp) != NULL) {/*从命名管道文件中读数据*/
+                printf("Received message from Server :%s \n", readbuf);
+                fclose(fp);
+            }
+            else {
+                if (ferror(fp)) {
+                    printf("read fifo failed.\n");
+                    exit(1);
+                }
+            }
+        }
+    }
+    // send msg
+    else {
+        while(1) {
+            fp = openfile(fp, "w");
+            printf("Wait for you send\n>> ");
+            char read_str[MAX_LEN];
+            scanf("%s", read_str);
+            if (fputs(read_str, fp) == EOF) {
+                printf("write fifo error. \n");
                 exit(1);
             }
+            fclose(fp);
         }
     }
     return 0;
