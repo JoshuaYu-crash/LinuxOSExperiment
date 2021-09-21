ed3d3b8bd79c9de2bb5764e307cf47ee3298d464
骇人的籽 <60039041+JoshuaYu-crash@users.noreply.github.com>
2021-09-21 21:47:32 +0800

    update: add ch2 work

---
+++ ch2/work/client.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+#define FIFO_FILE "/tmp/TEMPFIFO"/*命名管道的路径、文件名*/
+#define MAX_LEN 1024
+
+
+int main() {
+    FILE *fp;
+    int i;
+
+    if ((fp = fopen(FIFO_FILE, "w")) == NULL) {/*打开命名管道文件*/
+        printf("open fifo failed. \n");
+        exit(1);
+    }
+
+    while(1) {
+        printf("write : ");
+        char read_str[MAX_LEN];
+        scanf("%d", )
+        if (fputs(read_str, fp) == EOF) {
+            printf("write fifo error. \n");
+            exit(1);
+        }
+    }
+
+    fclose(fp);
+    return 0;
+
+}
+
---
+++ ch2/work/server.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/stat.h>
+#include <unistd.h>
+#include <linux/stat.h>
+
+#define FIFO_FILE "/tmp/TEMPFIFO" /*命名管道的路径、文件名*/
+
+
+// Create FIFO-FILE if not exists
+void init() {
+    if ((fp = fopen(FIFO_FILE, "r")) == NULL) {/*如果命名管道文件不存在，要先创建一个*/
+        umask(0);//清除文件创建时权限位的屏蔽作用
+        mknod(FIFO_FILE, S_IFIFO | 0666, 0);//创建FIFO文件
+        printf("Create new FIFO-FILE successed. \n");
+    } else {
+        fclose(fp);
+    }
+}
+
+
+int main() {
+    FILE *fp;
+    char readbuf[80];
+
+    init()
+
+    while (1) {
+        if ((fp = fopen(FIFO_FILE, "r")) == NULL) {/*打开命名管道文件*/
+            printf("open fifo failed. \n");
+            exit(1);
+        }
+
+        if (fgets(readbuf, 80, fp) != NULL) {/*从命名管道文件中读数据*/
+            printf("Received string :%s \n", readbuf);
+            fclose(fp);
+        } else {
+            if (ferror(fp)) {
+                printf("read fifo failed.\n");
+                exit(1);
+            }
+        }
+    }
+    return 0;
+}
